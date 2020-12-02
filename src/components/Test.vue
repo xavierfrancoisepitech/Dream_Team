ab7a9a5dcaa2cf82b50d723a7eb74ae5bf737360
lebiscuithd <72382518+lebiscuithd@users.noreply.github.com>
2020-12-03 00:04:03 +0100

    build:vue cli

---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h1 class="blue">Composant de test</h1>
+      <h2 v-for="post in posts" v-bind:key="post.id">{{ post.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  computed: {
+    posts () {
+      return this.$store.state.posts
+    }
+  },
+  mounted () {
+    this.$store.dispatch('getPosts')
+  }
+}
+</script>
+
+<style scoped>
+.blue {
+    color: blue;
+}
+</style>
