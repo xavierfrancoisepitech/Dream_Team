629350a95d2b0c294862c71be2653ab97eaad2bf
lebiscuithd <72382518+lebiscuithd@users.noreply.github.com>
2020-12-03 00:33:32 +0100

    build:add bootstrap vue

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
       <h1 class="blue">Composant de test</h1>
+          <b-avatar src="https://placekitten.com/300/300" size="6rem"></b-avatar>
       <h2 v-for="post in posts" v-bind:key="post.id">{{ post.title }}</h2>
+      <button class="btn btn-outline-secondary">Boo</button>
+      <button class="btn btn-primary"></button>
   </div>
 </template>
 
